4f8d3c73508aaf00143df40a310228e62d8cf050
Ramiro Morales <cramm0@gmail.com>
2015-12-26 12:45:51 -0300

    hack: Add tools/ subdir

---
+++ tools/chocolateyInstall.ps1
@@ -0,0 +1,22 @@
+﻿try
+{
+    $packageName = 'hack'
+    $url = 'https://github.com/chrissimpkins/Hack/releases/download/v2.018/Hack-v2_018-ttf.zip'
+
+    $destination = Join-Path $Env:Temp $packageName
+
+    Install-ChocolateyZipPackage $packageName -url $url -unzipLocation $destination
+
+    $shell = New-Object -ComObject Shell.Application
+    $fontsFolder = $shell.Namespace(0x14)
+
+    Get-ChildItem $destination -Recurse -Filter *.ttf |
+        ForEach-Object { $fontsFolder.CopyHere($_.FullName) }
+
+    Remove-Item $destination -Recurse
+}
+catch
+{
+    Write-ChocolateyFailure $packageName "$($_.Exception.Message)"
+    throw
+}
