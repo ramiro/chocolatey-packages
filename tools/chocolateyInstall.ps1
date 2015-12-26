1280aabc94fd6349aaf780e5f8c55666ab5b478f
Ramiro Morales <cramm0@gmail.com>
2015-12-26 12:49:20 -0300

    hack: Remove Write-ChocolateyFailure usage.

--- tools/chocolateyInstall.ps1
+++ tools/chocolateyInstall.ps1
@@ -1,22 +1,14 @@
-﻿try
-{
-    $packageName = 'hack'
-    $url = 'https://github.com/chrissimpkins/Hack/releases/download/v2.018/Hack-v2_018-ttf.zip'
+﻿$packageName = 'hack'
+$url = 'https://github.com/chrissimpkins/Hack/releases/download/v2.018/Hack-v2_018-ttf.zip'
 
-    $destination = Join-Path $Env:Temp $packageName
+$destination = Join-Path $Env:Temp $packageName
 
-    Install-ChocolateyZipPackage $packageName -url $url -unzipLocation $destination
+Install-ChocolateyZipPackage $packageName -url $url -unzipLocation $destination
 
-    $shell = New-Object -ComObject Shell.Application
-    $fontsFolder = $shell.Namespace(0x14)
+$shell = New-Object -ComObject Shell.Application
+$fontsFolder = $shell.Namespace(0x14)
 
-    Get-ChildItem $destination -Recurse -Filter *.ttf |
-        ForEach-Object { $fontsFolder.CopyHere($_.FullName) }
+Get-ChildItem $destination -Recurse -Filter *.ttf |
+    ForEach-Object { $fontsFolder.CopyHere($_.FullName) }
 
-    Remove-Item $destination -Recurse
-}
-catch
-{
-    Write-ChocolateyFailure $packageName "$($_.Exception.Message)"
-    throw
-}
+Remove-Item $destination -Recurse
